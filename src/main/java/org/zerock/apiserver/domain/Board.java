7d97f474377c1768f742788b23758b50e0a9ae9f
gangvia <GangHyoun>
2025-01-08 11:03:45 +0900

    Feat: Entity_board, member, region
    Entity & Repository (Board,Member,Region)

---
+++ src/main/java/org/zerock/apiserver/domain/Board.java
@@ -0,0 +1,27 @@
+package org.zerock.apiserver.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> post = new ArrayList<>();
+}
