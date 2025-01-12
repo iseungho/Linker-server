fa9b79239f8f98934f0806e4b5c7fd19eba23522
gangvia <GangHyoun>
2025-01-12 21:31:51 +0900

    Feat: Json Update
    Json, Region, category db update

--- src/main/java/org/zerock/apiserver/service/RegionService.java
+++ src/main/java/org/zerock/apiserver/service/RegionService.java
@@ -2,4 +2,5 @@ package org.zerock.apiserver.service;
 
 public interface RegionService {
     void initializeRegion(Long id, String name, Long parentRegionId);
+    void addRegion(String name, Long parentRegionId);
 }
